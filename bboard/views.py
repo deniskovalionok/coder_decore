f43628029baf7b559704c350c1c7785fc05c69e9
denis <deni-kov@mail.ru>
2022-04-21 21:06:49 +0300

    create CD

---
+++ bboard/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+
+def index(request):
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs':bbs})
